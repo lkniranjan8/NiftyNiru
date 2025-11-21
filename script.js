cc49ec3269431dadf105c70a169694d027ad4727
NiftyNiru <lkniranjan8@gmail.com>
2025-11-21 20:14:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 document.getElementById("year").textContent = new Date().getFullYear();
-
-const toggle = document.getElementById("menuToggle");
-const nav = document.getElementById("navMenu");
-
-toggle.addEventListener("click", () => {
-  nav.classList.toggle("open");
-});
